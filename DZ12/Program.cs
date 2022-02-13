8415372120ddfbb778df130fa19d14b6c042f740
kzlager <highcolour@yandex.ru>
2022-02-13 18:26:01 +0300

    adding files

---
+++ DZ12/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 12: Удалить вторую цифру трёхзначного числа.
+int GetRandom()
+{
+    int number = new Random().Next(100,1000);
+    return number;
+}
+int randomazer = GetRandom();
+Console.WriteLine($"Случайное число: {randomazer}");
+int resultOne = randomazer / 100;
+int resultTwo = randomazer % 10;
+
+Console.WriteLine($"{resultOne}.{resultTwo}");
+
